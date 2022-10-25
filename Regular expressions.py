f604d09b152cb785904b81eaf6e1d12cf0abb48e
Maksimilian Romanov <maks.romanov.15@inbox.ru>
2022-10-25 22:06:41 +0300

    add

--- Regular expressions.py
+++ Regular expressions.py
@@ -37,27 +37,29 @@ name = re.findall(pattern_name, new_text1)
 new_text2 = re.sub(pattern_name, substition_name, new_text1)
 
 #Объединим дублируемые записи
-pattern_duble1 = r'(Мартиняхин Виталий Геннадьевич,ФНС,cоветник отдела Интернет проектов Управления информационных технологий)(,+)'
+pattern_duble1 = r'([А-Я][а-я]+(\s|,)[А-Я][а-я]+(\s|,)[А-Я][а-я]+,ФНС,cоветник отдела Интернет проектов Управления информационных технологий)(,+)'
 double1 = re.findall(pattern_duble1, new_text2)
 substition_duble1 = r'\1 +7(495)913-00-37,'
 new_text3 = re.sub(pattern_duble1, substition_duble1, new_text2)
 
-pattern_duble2 = r'(Лагунцов Иван Алексеевич),+(Минфин,+)(\+7\(495\)913-11-11)\s(доб\.0792)()'
+pattern_duble2 = r'([А-Я][а-я]+(\s|,)[А-Я][а-я]+(\s|,)[А-Я][а-я]+),+(Минфин,+)(\+7\(495\)913-11-11)\s(доб\.0792)()'
 substition_duble2 = r'\1\2\3\4\5, Ivan.Laguntcov@minfin.ru'
 double2 = re.findall(pattern_duble2, new_text3)
 new_text4 = re.sub(pattern_duble2, substition_duble2, new_text3)
 
 new_text4 = new_text4.replace(',,,', ',').replace(',,', ',')
 
-pattern_del_str1 = r'(Лагунцов Иван,Ivan.Laguntcov@minfin.ru,)'
+pattern_del_str1 = r'([А-Я][а-я]+(\s|,)[А-Я][а-я]+,+Ivan.Laguntcov@minfin.ru,)'
 substition_del1 = " "
 str1 = re.findall(pattern_del_str1, new_text4)
 new_text5 = re.sub(pattern_del_str1, substition_del1, new_text4)
 
-pattern_del_str2 = r'(Мартиняхин Виталий Геннадьевич,ФНС,\+7\(495\)913-00-37)'
+pattern_del_str2 = r'([А-Я][а-я]+(\s|,)[А-Я][а-я]+(\s|,)[А-Я][а-я]+,ФНС,\+7\(495\)913-00-37)'
 substition_del2 = ""
 str2 = re.findall(pattern_del_str2, new_text5)
 new_text6 = re.sub(pattern_del_str2, substition_del2, new_text5)
 new_text6 = new_text6.replace(', ', '').replace(',,', ', ').replace(',', ', ')
 pprint(new_text6)
 
+with open ("right_phonebook_raw.txt", 'w', encoding='utf8') as right_phonebook:
+    right_phonebook.write(new_text6)
